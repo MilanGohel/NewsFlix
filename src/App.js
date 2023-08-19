39144c612571acb53957d8a2f700850515730e6a
MilanGohel <milan.gohel66@gmail.com>
2023-08-19 18:07:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState} from 'react'
+// import LoadingBar from 'react-top-loading-bar'
+import scroll from './App.css'
+import { Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import News from './components/News'
+import { BrowserRouter as Router,Routes } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar';
+ 
+function App(){
+  const [mode,setMode] = useState('light');
+  const [progress,setProgress] = useState(0);
+  const toggleMode = ()=>{
+    // if(mode==='dark'){
+    //   setMode('light')
+    // }
+    // else{
+    //   setMode('dark')
+    // }
+    mode==='dark'?setMode('light'):setMode('dark');
+  }
+  return <>
+    <div>
+          <LoadingBar
+          color='#f11946'
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+          height={4}
+          shadow={true}
+          />
+        <Router>
+        <NavBar mode={mode} toggleMode={toggleMode}/>
+          <Routes>
+          <Route exact key='general' path='/' style={scroll} element={<News setProgress={setProgress} key='home' country='in' pageSize={12} category='general' mode={mode}/>}/>
+          <Route exact path='/entertainment' element={<News setProgress={setProgress} key='entertainment' country='in' pageSize={12} category='entertainment' mode={mode}/>}/>
+          <Route exact path='/health' element={<News setProgress={setProgress} key='health' country='in' pageSize={12} category='health' mode={mode}/>}/>
+          <Route exact path='/business' element={<News setProgress={setProgress} key='business' country='in' pageSize={12} category='business' mode={mode}/>}/>
+          <Route exact path='/business' element={<News setProgress={setProgress} key='business' country='in' pageSize={12} category='business' mode={mode}/>}/>
+          <Route exact path='/science' element={<News setProgress={setProgress} key='science' country='in' pageSize={12} category='science' mode={mode}/>}/>
+          <Route exact path='/sports' element={<News setProgress={setProgress} key='sports' country='in' pageSize={12} category='sports' mode={mode}/>}/>
+          <Route exact path='/technology' element={<News setProgress={setProgress} key='technology' country='in' pageSize={12} category='technology' mode={mode}/>}/>
+          </Routes>
+        </Router>   
+      </div> 
+      </>
 }
 
-export default App;
+export default App
+
+
